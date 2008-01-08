41e7ef00d47be96a1fcbb5a5e140de987d0c2140
Eric Wong <normalperson@yhbt.net>
2008-01-07 16:27:08 -0800

    fast_xs_monkey_patcher: rewrite to make this more readable for me
    I have the attention span of a gold fish and I use 80x24 terminals.

--- lib/fast_xs_monkey_patcher.rb
+++ lib/fast_xs_monkey_patcher.rb
@@ -1,70 +1,32 @@
 require 'fast_xs'
 
-if defined?(CGI)
+class CGI
+  def CGI::escapeHTML(value); value.fast_xs_html; end
+  def CGI::escape(value); value.fast_xs_cgi; end
+  def CGI::unescape(value); value.fast_uxs_cgi; end
+end if defined?(CGI)
 
-  class CGI
+module ERB::Util
+  def html_escape(value); value.to_s.fast_xs_html; end
+  alias h html_escape
+  module_function :h
+  module_function :html_escape
 
-    def CGI::escapeHTML(value)
-      value.fast_xs_html
-    end
+  def url_encode(value); value.to_s.fast_xs_url; end
+  alias u url_encode
+  module_function :u
+  module_function :url_encode
+end if defined?(ERB::Util)
 
-    def CGI::escape(value)
-      value.fast_xs_cgi
-    end
+class Mongrel::HttpRequest
+  def self.unescape(s); s.fast_uxs_cgi; end
+  def self.escape(s); s.to_s.fast_xs_cgi; end
+end if defined?(Mongrel::HttpRequest)
 
-    def CGI::unescape(value)
-      value.fast_uxs_cgi
-    end
+module Rack::Utils
+  def unescape(s); s.fast_uxs_cgi; end
+  module_function :unescape
 
-  end
-
-end
-
-if defined?(ERB::Util)
-
-  module ERB::Util
-
-    def html_escape(value)
-      value.to_s.fast_xs_html
-    end
-    alias h html_escape
-    module_function :h
-    module_function :html_escape
-
-    def url_encode(value)
-      value.to_s.fast_xs_url
-    end
-    alias u url_encode
-    module_function :u
-    module_function :url_encode
-
-  end
-
-end
-
-if defined?(Mongrel::HttpRequest)
-
-  class Mongrel::HttpRequest
-
-    def self.unescape(s)
-      s.fast_uxs_cgi
-    end
-
-    def self.escape(s)
-      s.to_s.fast_xs_cgi
-    end
-
-  end
-
-end
-
-if defined?(Rack::Utils)
-  module Rack::Utils
-    def unescape(s); s.fast_uxs_cgi; end
-    module_function :unescape
-
-    def escape(s); s.to_s.fast_xs_cgi; end
-    module_function :escape
-  end
-end
-
+  def escape(s); s.to_s.fast_xs_cgi; end
+  module_function :escape
+end if defined?(Rack::Utils)
