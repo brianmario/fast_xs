f168d56575a278903ab9800a1007431b0d9e8348
Eric Wong <normalperson@yhbt.net>
2009-07-30 19:16:58 -0700

    monkey_patcher: disable rdoc
    Don't kill documentation for existing classes

--- lib/fast_xs_monkey_patcher.rb
+++ lib/fast_xs_monkey_patcher.rb
@@ -1,33 +1,40 @@
 require 'fast_xs'
 require 'fast_xs_extra'
 
-class CGI
+class CGI # :nodoc:
   def CGI::escapeHTML(value); value.fast_xs_html; end
   def CGI::escape(value); value.fast_xs_cgi; end
   def CGI::unescape(value); value.fast_uxs_cgi; end
 end if defined?(CGI)
 
-module ERB::Util
-  def html_escape(value); value.to_s.fast_xs_html; end
-  alias h html_escape
-  module_function :h
-  module_function :html_escape
+class ERB # :nodoc:
+  module Util # :nodoc:
 
-  def url_encode(value); value.to_s.fast_xs_url; end
-  alias u url_encode
-  module_function :u
-  module_function :url_encode
+    def html_escape(value); value.to_s.fast_xs_html; end
+    alias h html_escape
+    module_function :h
+    module_function :html_escape
+
+    def url_encode(value); value.to_s.fast_xs_url; end
+    alias u url_encode
+    module_function :u
+    module_function :url_encode
+  end
 end if defined?(ERB::Util)
 
-class Mongrel::HttpRequest
-  def self.unescape(s); s.fast_uxs_cgi; end
-  def self.escape(s); s.to_s.fast_xs_cgi; end
+class Mongrel # :nodoc:
+  class HttpRequest # :nodoc:
+    def self.unescape(s); s.fast_uxs_cgi; end
+    def self.escape(s); s.to_s.fast_xs_cgi; end
+  end
 end if defined?(Mongrel::HttpRequest)
 
-module Rack::Utils
-  def unescape(s); s.fast_uxs_cgi; end
-  module_function :unescape
+module Rack # :nodoc:
+  module Utils # :nodoc:
+    def unescape(s); s.fast_uxs_cgi; end
+    module_function :unescape
 
-  def escape(s); s.to_s.fast_xs_cgi; end
-  module_function :escape
+    def escape(s); s.to_s.fast_xs_cgi; end
+    module_function :escape
+  end
 end if defined?(Rack::Utils)
