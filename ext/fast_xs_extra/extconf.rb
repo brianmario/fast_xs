8f69ed57dadc8d121fb3cd1361d119d262ba56ec
Eric Wong <normalperson@yhbt.net>
2009-07-30 19:13:34 -0700

    fast_xs_extra/extconf: compatibility with rake-compiler

--- ext/fast_xs_extra/extconf.rb
+++ ext/fast_xs_extra/extconf.rb
@@ -1,5 +1,5 @@
 require 'mkmf'
 have_header('assert.h') or exit
-find_header('gcc.h', '../fast_xs')
+find_header('gcc.h', File.dirname(__FILE__) + '/../fast_xs')
 dir_config('fast_xs_extra')
 create_makefile('fast_xs_extra')
