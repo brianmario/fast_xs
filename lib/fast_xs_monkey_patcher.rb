447fefb2b4a43f1628e5f47a7d60b9669d1d4dcc
Eric Wong <normalperson@yhbt.net>
2011-01-25 16:22:24 -0800

    quiet down warnings about redefining methods
    Yes it's evil, yes we do it.

--- lib/fast_xs_monkey_patcher.rb
+++ lib/fast_xs_monkey_patcher.rb
@@ -2,13 +2,20 @@ require 'fast_xs'
 require 'fast_xs_extra'
 
 class CGI # :nodoc:
-  def CGI::escapeHTML(value); value.fast_xs_html; end
-  def CGI::escape(value); value.fast_xs_cgi; end
-  def CGI::unescape(value); value.fast_uxs_cgi; end
+  class << self
+    undef_method :escapeHTML
+    undef_method :escape
+    undef_method :unescape
+    def escapeHTML(value); value.fast_xs_html; end
+    def escape(value); value.fast_xs_cgi; end
+    def unescape(value); value.fast_uxs_cgi; end
+  end
 end if defined?(CGI)
 
 class ERB # :nodoc:
   module Util # :nodoc:
+    undef_method :html_escape
+    undef_method :url_encode
 
     def html_escape(value); value.to_s.fast_xs_html; end
     alias h html_escape
@@ -24,16 +31,22 @@ end if defined?(ERB::Util)
 
 module Mongrel # :nodoc:
   class HttpRequest # :nodoc:
-    def self.unescape(s); s.fast_uxs_cgi; end
-    def self.escape(s); s.to_s.fast_xs_cgi; end
+    class << self
+      undef_method :unescape
+      undef_method :escape
+      def unescape(s); s.fast_uxs_cgi; end
+      def escape(s); s.to_s.fast_xs_cgi; end
+    end
   end
 end if defined?(Mongrel::HttpRequest)
 
 module Rack # :nodoc:
   module Utils # :nodoc:
+    undef_method :unescape
     def unescape(s); s.fast_uxs_cgi; end
     module_function :unescape
 
+    undef_method :escape
     def escape(s); s.to_s.fast_xs_cgi; end
     module_function :escape
   end
